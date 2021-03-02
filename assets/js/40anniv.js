dffcbe7772954306f85f404bf0f81d4f74243ba3
yutingh <yutingh@cw.com.tw>
2021-03-02 18:36:08 +0800

    js更新email填入方式

--- assets/js/40anniv.js
+++ assets/js/40anniv.js
@@ -193,3 +193,13 @@ if ( window.location.href.indexOf('initial') > 0 ) {
     })
 }
 
+if ( window.location.href.indexOf('&email=') > 0 ) {
+    var email = window.location.href.split('email=')[1];
+    $(window).on('load', function(){
+        $('a.unsubscribe').each(function(){
+            var originHref = $(this).attr('href'),
+                unsubscribeHref = originHref.replace('%%email%%', email);
+            $(this).attr('href', unsubscribeHref);
+        })
+    })
+}
